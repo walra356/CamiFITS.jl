5a3f0101cbbecc0fc04d91d73be432ca2a25662b
Jook Walraven <69215586+walra356@users.noreply.github.com>
2023-03-10 22:31:09 +0100

    work on docs

--- src/CamiFITS.jl
+++ src/CamiFITS.jl
@@ -1,5 +1,119 @@
 module CamiFITS
 
-# Write your package code here.
+export FITS_HDU
+export FITS_header
+export FITS_data
+export FITS_table
+export parse_FITS_TABLE
+export FITS_name
+export cast_FITS_name
+
+export fdiff_weight
+export fdiff_expansion_weights
+export fdiff_expansion
+export fwd_diff_expansion_weights
+export fdiff_interpolation_expansion_coeffs
+export fdiff_interpolation
+export fdiff_differentiation_expansion_coeffs
+export fdiff_differentiation
+export create_lagrange_differentiation_matrix
+export fdiff_adams_moulton_expansion_coeff
+export fdiff_adams_moulton_expansion_coeffs
+export create_adams_moulton_weights
+export fdiff_adams_bashford_expansion_coeffs
+export trapezoidal_epw
+export trapezoidal_integration
+
+export matG
+export matσ
+export matMinv
+export OUTSCH
+export OUTSCH_WKB
+export OUTSCH_WJ
+export Adams
+export castAdams
+export updateAdams!
+export INSCH
+export INSCH_WKB
+export INSCH_WJ
+export adams_moulton_inward
+export adams_moulton_outward
+export adams_moulton_normalized
+export adams_moulton_patch
+export count_nodes
+export adams_moulton_solve
+export adams_moulton_prepare
+export adams_moulton_iterate
+export adams_moulton_master
+export demo_hydrogen
+export hydrogenic_reduced_wavefunction
+export RH1s
+export RH2s
+export RH2p
+export restore_wavefunction
+export reduce_wavefunction
+
+export Grid
+export gridname
+export gridfunction
+export castGrid
+export findIndex
+export autoRmax
+export autoNtot
+export autoPrecision
+export autoSteps
+export autoGrid
+export grid_differentiation
+export grid_integration
+
+export get_Na
+export get_Nb
+export get_Nlctp
+export get_Nmin
+export get_Nuctp
+export Pos
+export Def
+export castDef
+export initE
+
+export fits_create
+export fits_read
+export fits_extend
+export fits_info
+export fits_copy
+export fits_combine
+export fits_add_key
+export fits_edit_key
+export fits_delete_key
+export fits_rename_key
+
+#export plot_matrices
+#export plot!
+export step125
+export select125
+export edges
+export steps
+export stepcenters
+export stepedges
+#export centerticks
+#export edgeticks
+#export centers
+#export edges
+
+export FORTRAN_format
+export cast_FORTRAN_format
+export cast_FORTRAN_datatype
+
+
+include("fits_pointers.jl")
+include("read_io.jl")
+include("write_io.jl")
+include("fits_objects.jl")
+include("fits_private_sector.jl")
+include("fits_public_sector.jl")
+include("plot_private_sector.jl")
+include("plot_public_sector.jl")
+include("Header-and-Data-Input.jl")
+include("FORTRAN.jl")
 
 end
