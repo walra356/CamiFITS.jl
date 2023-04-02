d3f7f135a1077cc3d029e7eec919b0576911dc19
Jook Walraven <69215586+walra356@users.noreply.github.com>
2023-04-02 23:09:00 +0200

    work on docs

--- src/fits_objects.jl
+++ src/fits_objects.jl
@@ -96,6 +96,7 @@ Object to hold the card information of the [`FITS_header`](@ref) object.
 
 The fields are:
 * `.cardindex`:  identifier of the header record (`::Int`)
+* `.record`:  the full record on the card (`::String`)
 * `.keyword`:  name of the corresponding header record (`::String`)
 * `.val`:  value  of the corresponding header record (`::Any`)
 * `.comment`:  comment on the corresponding header record (`::String`)
