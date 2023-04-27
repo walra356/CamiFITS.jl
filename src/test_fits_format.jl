abcd1f434b6e88755caa0b79af5b83af22549b7b
Jook Walraven <69215586+walra356@users.noreply.github.com>
2023-04-27 08:30:41 +0200

    work on docs

--- src/test_fits_format.jl
+++ src/test_fits_format.jl
@@ -451,21 +451,27 @@ COMMENT, CROTAn, CRPIXn, CRVALn, CTYPEn, DATAMAX, DATAMIN, DATE, DATE-OBS,
 END, EPOCH, EQUINOX, EXTEND, EXTLEVEL, EXTNAME, EXTVER, GCOUNT, GROUPS,
 HISTORY, INSTRUME, NAXIS, NAXISn, OBJECT, OBSERVER, ORIGIN, PCOUNT, PSCALn,
 PTYPEn, PZEROn, REFERENC, SIMPLE, TBCOLn, TDIMn, TDISPn, TELESCOP, TFIELDS,
-TFORMn, THEAP, TNULLn, TSCALn, TTYPEn, TUNITn, TZEROn, XTENSION.
+TFORMn, THEAP, TNULLn, TSCALn, TTYPEn, TUNITn, TZEROn, XTENSION,
+
+where `n = 1,...,nmax` as specified for the keyword. Use `keyword = "ALL"` to dump
+a list of all keyword descriptions.
 
 The descriptions are based on appendix C to [FITS standard - version 4.0](https://fits.gsfc.nasa.gov/fits_standard.html),
 which is *not part of the standard but included for convenient reference*.
 ```
-julia> fits_keyword("END");
-KEYWORD:    END
+julia> fits_keyword("NAXISn");
+KEYWORD:    NAXISn
 REFERENCE:  FITS Standard - version 4.0 - Appendix C
 CLASS:      general
 STATUS:     mandatory
 HDU:        primary, groups, extension, array, image, ASCII-table, bintable,
-VALUE:      none
-DEFAULT:    none
-COMMENT:    marks the end of the header keywords
-DEFINITION: This keyword has no associated value.  Columns 9-80 shall be filled with ASCII blanks.
+VALUE:      integer
+DEFAULT:    RANGE:      [0:]
+COMMENT:    size of the axis
+DEFINITION: The value field of this indexed keyword shall contain a non-negative integer, representing the number of elements along axis n of a data array.
+The NAXISn must be present for all values n = 1,...,NAXIS, and for no other values of n.
+A value of zero for any of the NAXISn signifies that no data follow the header in the HDU.
+If NAXIS is equal to 0, there should not be any NAXISn keywords.
 
 julia> fits_keyword()
 FITS defined keywords:
