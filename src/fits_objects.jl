f4832214845ac3c0f6a6d0d973437f32ffe1e0ea
Jook Walraven <69215586+walra356@users.noreply.github.com>
2023-06-07 20:51:20 +0200

    work on docs

--- src/fits_objects.jl
+++ src/fits_objects.jl
@@ -226,7 +226,24 @@ Create the [`FITS_header`](@ref) object from the dataobject. The
 dataobject-input mode is used by [`fits_create`](@ref) to ceate the header
 object as part of creating the [`FITS`](@ref) object starting from Julia data 
 input.
+#### Example:
+```
+julia> data = [11 21 31; 12 22 23; 13 23 33];
 
+julia> d = cast_FITS_dataobject("image", data);
+
+julia> h = cast_FITS_header(d);
+
+julia> h.map
+Dict{String, Int64} with 7 entries:
+  "BITPIX"   => 2
+  "NAXIS2"   => 5
+  "XTENSION" => 1
+  "NAXIS1"   => 4
+  ""         => 36
+  "NAXIS"    => 3
+  "END"      => 6
+```
     cast_FITS_header(record::Vector{String})
 
 Create the [`FITS_header`](@ref) object from a block of (a multiple of) 36 
@@ -235,22 +252,20 @@ is used by [`fits_read`](@ref) after reading the header records from disk
 (see casting diagram above).
 #### Example:
 ```
-julia> record = [rpad("key$i",8) * ''' * rpad("$i",70) * ''' for i=1:36]
-36-element Vector{String}:
- "key1    '1                     " ⋯ 18 bytes ⋯ "                              '"
- "key2    '2                     " ⋯ 18 bytes ⋯ "                              '"
- "key3    '3                     " ⋯ 18 bytes ⋯ "                              '"
- ⋮
- "key35   '35                    " ⋯ 18 bytes ⋯ "                              '"
- "key36   '36                    " ⋯ 18 bytes ⋯ "                              '"
+julia> record = [rpad("KEYWORD$i",8) * "'" * rpad("$i",70) * "'" for i=1:3];
 
-julia> header = cast_FITS_header(record);
+julia> blanks = [repeat(' ', 80) for i = 1:36-length(record)];
 
-julia> header.map["key35"]
-35
+julia> append!(record, blanks);
 
-julia> header.card[35].keyword
-"key35                                                                   '"
+julia> h = cast_FITS_header(record);
+
+julia> h.map
+Dict{String, Int64} with 4 entries:
+  "KEYWORD3" => 3
+  "KEYWORD2" => 2
+  "KEYWORD1" => 1
+  ""         => 36                                                                '"
 ```
 """
 function cast_FITS_header(dataobject::FITS_dataobject)
