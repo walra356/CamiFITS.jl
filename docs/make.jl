b8b11cb17d0a3125a20e32b2efefec88c986f31f
Jook Walraven <69215586+walra356@users.noreply.github.com>
2023-07-03 10:09:21 +0200

    wwork on docs

--- docs/make.jl
+++ docs/make.jl
@@ -2,14 +2,14 @@ using Documenter
 using CamiFITS
 
 makedocs(;
-    modules=[CamiFITS],
-    authors="<walra356@planet.nl> and contributors",
+    modules = [CamiFITS],
+    authors = "<walra356@planet.nl> and contributors",
     repo = "github.com/walra356/CamiFITS.jl.git",
-    sitename="CamiFITS.jl",
+    sitename = "CamiFITS.jl",
     format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        #canonical="https://walra356.github.io/CamiFITS.jl",
-        assets=String[]
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://walra356.github.io/CamiFITS.jl",
+        assets = String[]
     ),
     pages=[
         "Home" => "index.md",
