519aaa2124fd15f6b1937dfa938550e0ceb38809
walra356 <walra356@planet.nl>
2024-09-02 10:04:24 +0200

    work on docs

--- docs/make.jl
+++ docs/make.jl
@@ -4,11 +4,9 @@ using CamiFITS
 makedocs(;
     modules = [CamiFITS],
     authors = "<walra356@planet.nl> and contributors",
-    #repo = "github.com/walra356/CamiFITS.jl.git",
     sitename = "CamiFITS.jl",
     format=Documenter.HTML(;
         prettyurls = get(ENV, "CI", "false") == "true",
-        #repolink = "https://walra356.github.io/CamiFITS.jl",
         assets = String[]
     ),
     pages=[
