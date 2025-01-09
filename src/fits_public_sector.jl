5c4de37c5d9e6c21064c3521810789fdc8671662
walra356 <walra356@planet.nl>
2025-01-09 13:17:50 +0100

    work on pointers

--- src/fits_public_sector.jl
+++ src/fits_public_sector.jl
@@ -533,24 +533,25 @@ summay of the pointers used by `CamiFITS`
 ```
 julia> filnam = "kanweg.fits";
 
+julia> data = [0x0000043e, 0x0000040c, 0x0000041f];
+
 julia> f = fits_create(filnam, data; protect=false);
 
 julia> fits_extend!(f; hdutype="'IMAGE   '");
 
 julia> fits_extend!(filnam, data; hdutype="'IMAGE   '");
 
-julia> fits_pointers(filnam)
-10-element Vector{String}:
- "             block count: 5"
- "               hdu count: 3"
- " start-of-block pointers: (0, 2880, 5760, 8640, 11520)"
- "   end-of-block pointers: (2880, 5760, 8640, 11520, 14400)"
- "   start-of-hdu pointers: (0, 5760, 8640)"
- "     end-of-hdu pointers: (2880, 8640, 11520)"
- "start-of-header pointers: (0, 5760, 8640)"
- "  end-of-header pointers: (2880, 8640, 11520)"
- "  start-of-data pointers: (2880, 8640, 11520)"
- "    end-of-data pointers: (5760, 8640, 14400)"
+julia> foreach(println, fits_pointers(filnam))
+             block count: 5
+               hdu count: 3
+ start-of-block pointers: (0, 2880, 5760, 8640, 11520)
+   end-of-block pointers: (2880, 5760, 8640, 11520, 14400)
+   start-of-hdu pointers: (0, 5760, 8640)
+     end-of-hdu pointers: (2880, 8640, 11520)
+start-of-header pointers: (0, 5760, 8640)
+  end-of-header pointers: (2880, 8640, 11520)
+  start-of-data pointers: (2880, 8640, 11520)
+    end-of-data pointers: (5760, 8640, 14400)
 ```
 """
 function fits_pointers(filnam::String) 
